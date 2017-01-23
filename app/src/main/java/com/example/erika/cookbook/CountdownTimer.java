81d6d67481723dc1b234cd9ae725242f721b8289
ciz0031 <erika.cizova.cz@gmail.com>
2017-01-23 23:05:17 +0100

    foregroundService implemented.

--- app/src/main/java/com/example/erika/cookbook/CountdownTimer.java
+++ app/src/main/java/com/example/erika/cookbook/CountdownTimer.java
@@ -1,9 +1,12 @@
 package com.example.erika.cookbook;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.CountDownTimer;
@@ -35,11 +38,9 @@ public class CountdownTimer extends Activity {
     private RelativeLayout RL;
     private ProgressBar progressBar;
     private Button addOneMinuteButton;
-    MyCountDownTimer myCountDownTimer;
-    int dobaPeceni;
+    int dobaPeceni, dobaPeceniExtras;
     public NotificationManager manager;
     public NotificationCompat.Builder builder;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,60 +71,100 @@ public class CountdownTimer extends Activity {
         builder.setSound(alarmSound);
         manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        dobaPeceniTP = new TimePicker(CountdownTimer.this);
+        dobaPeceniTP.setIs24HourView(true);
+        dobaPeceniTP.setTag("timePickDobaPeceni");
+        RL.addView(dobaPeceniTP);
+
+        dobaPeceniTP.setVisibility(View.INVISIBLE);
 
         //podle toho odkud se aktivita vola se vytvori UI - kdyz z receptu, bude tam textView, kdyz z menu, bude tam timePicker
-        extras = getIntent().getExtras();
-        if(extras != null){
-            //textView s moznosti pridani 1 minuty k casu
-            dobaPeceni = extras.getInt("doba_peceni");
-            dobaPeceniTV.setText(String.valueOf(dobaPeceni));
-        }else {
-            progressBar.setVisibility(View.INVISIBLE);
-            dobaPeceni = 0;
-            //timePicker
-            dobaPeceniTP = new TimePicker(CountdownTimer.this);
-            dobaPeceniTP.setIs24HourView(true);
-            dobaPeceniTP.setCurrentHour(0);
-            dobaPeceniTP.setCurrentMinute(0);
-            dobaPeceniTP.setTag("timePickDobaPeceni");
-            RL.addView(dobaPeceniTP);
+
+        if (isMyServiceRunning(MyService.class) == true){
+            progressBar.setVisibility(View.VISIBLE);
+            addOneMinuteButton.setVisibility(View.VISIBLE);
+            startButton.setVisibility(View.INVISIBLE);
+            startButton.setClickable(false);
+            stopButton.setVisibility(View.VISIBLE);
+            stopButton.setClickable(true);
+            Log.d("service", "service running!");
+        }else if (isMyServiceRunning(MyService.class) == false){
+            Log.d("service", "service not running!");
+            extras = getIntent().getExtras();
+            if(extras != null){
+                //textView s moznosti pridani 1 minuty k casu
+                dobaPeceniExtras = extras.getInt("doba_peceni");
+                dobaPeceni = dobaPeceniExtras;
+                dobaPeceniTV.setText(String.valueOf(dobaPeceni));
+            }else {
+                progressBar.setVisibility(View.INVISIBLE);
+                dobaPeceniTP.setVisibility(View.VISIBLE);
+                dobaPeceni = 0;
+                dobaPeceniTP.setCurrentHour(0);
+                dobaPeceniTP.setCurrentMinute(0);
+
+            }
         }
+
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
                 addOneMinuteButton.setVisibility(View.VISIBLE);
+                addOneMinuteButton.setClickable(true);
+                dobaPeceniTP.setVisibility(View.INVISIBLE);
+                dobaPeceniTV.setVisibility(View.VISIBLE);
                 //Log.d("dobaPeceni", dobaPeceni);
-                if (dobaPeceni != 0){
-                    myCountDownTimer = new MyCountDownTimer(dobaPeceni*60*1000, 1000);
-                    //progressBar.setMax(dobaPeceni*60);
+                Bundle dataDobaPeceni = new Bundle();
+                if (dobaPeceniExtras != 0){
+                    dataDobaPeceni.putInt("doba_peceni", dobaPeceni*60*1000);
                 }else{
                     int hour = dobaPeceniTP.getCurrentHour();
                     int minutes = dobaPeceniTP.getCurrentMinute();
                     dobaPeceni = (hour * 60) + minutes;
-                    myCountDownTimer = new MyCountDownTimer(dobaPeceni*60*1000, 1000);
+                    dataDobaPeceni.putInt("doba_peceni", dobaPeceni*60*1000);
                     dobaPeceniTP.setVisibility(View.INVISIBLE);
                 }
+                Log.d("dobaPeceni", String.valueOf(dobaPeceni));
                 startButton.setVisibility(View.INVISIBLE);
                 startButton.setClickable(false);
                 stopButton.setVisibility(View.VISIBLE);
                 stopButton.setClickable(true);
-
                 progressBar.setMax(dobaPeceni * 60);
-                myCountDownTimer.start();
+
+                Intent i = new Intent(CountdownTimer.this, MyService.class);
+                i.putExtras(dataDobaPeceni);
+                startService(i);
             }
         });
 
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                myCountDownTimer.cancel();
+                stopService(new Intent(CountdownTimer.this, MyService.class));
                 stopButton.setVisibility(View.INVISIBLE);
                 stopButton.setClickable(false);
                 startButton.setVisibility(View.VISIBLE);
                 startButton.setClickable(true);
                 manager.cancelAll();
+                dobaPeceniTV.setVisibility(View.INVISIBLE);
+                if (dobaPeceniExtras == 0){
+                    progressBar.setVisibility(View.INVISIBLE);
+                    dobaPeceniTP.setVisibility(View.VISIBLE);
+                    dobaPeceniTP.setCurrentHour(0);
+                    dobaPeceniTP.setCurrentMinute(0);
+
+                    addOneMinuteButton.setVisibility(View.INVISIBLE);
+                    addOneMinuteButton.setClickable(false);
+                }else if (dobaPeceniExtras != 0){
+                    progressBar.setVisibility(View.INVISIBLE);
+                    dobaPeceniTP.setVisibility(View.VISIBLE);
+                    dobaPeceniTP.setCurrentHour(0);
+                    dobaPeceniTP.setCurrentMinute(dobaPeceni);
+                    addOneMinuteButton.setVisibility(View.INVISIBLE);
+                    addOneMinuteButton.setClickable(false);
+                }
             }
         });
 
@@ -132,7 +173,7 @@ public class CountdownTimer extends Activity {
             public void onClick(View view) {
                 //pridani jedne minuty k casu odpoctu !
                 manager.cancelAll();
-                myCountDownTimer.cancel();
+                stopService(new Intent(CountdownTimer.this, MyService.class));
                 startButton.setVisibility(View.INVISIBLE);
                 startButton.setClickable(false);
                 stopButton.setVisibility(View.VISIBLE);
@@ -144,25 +185,77 @@ public class CountdownTimer extends Activity {
                 int minutes = Integer.parseInt(minute) * 60;//prevedeno na sekundy
                 int dobaPeceniINT = minutes + seconds + 60;//v sekundach
                 progressBar.setMax(dobaPeceniINT);
-                myCountDownTimer = new MyCountDownTimer(dobaPeceniINT*1000, 1000);
-                myCountDownTimer.start();
+                Bundle dataDobaPeceni = new Bundle();
+                dataDobaPeceni.putInt("doba_peceni", dobaPeceniINT*1000);
+                Intent i = new Intent(CountdownTimer.this, MyService.class);
+                i.putExtras(dataDobaPeceni);
+                startService(i);
             }
         });
 
     }
-    public class MyCountDownTimer extends CountDownTimer {
-
-        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
-        }
-
-
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
-        public void onTick(long millisUntilFinished) {
+        public void onReceive(Context context, Intent intent) {
+            updateGUI(intent); // or whatever method used to update your GUI fields
+        }
+    };
 
-            int progress = (int) (millisUntilFinished/1000);//pocita od zhora dolu, po jednicku
-            long millis = millisUntilFinished;
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        //registerReceiver(broadcastReceiver, new IntentFilter(MyService.str_receiver));
+        Log.d("back button", "back button pressed");
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(broadcastReceiver, new IntentFilter(MyService.str_receiver));
+        Log.d("receiver", "Registered broacast receiver-onresume");
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
+        Log.d("receiver", "Unregistered broacast receiver-onpause");
+    }
+
+    @Override
+    public void onStop() {
+        try {
+            unregisterReceiver(broadcastReceiver);
+            Log.d("receiver", "Unregistered broacast receiver-onstop");
+        } catch (Exception e) {
+            // Receiver was probably already stopped in onPause()
+        }
+        super.onStop();
+    }
+    /*@Override
+    public void onDestroy() {
+        stopService(new Intent(this, MyService.class));
+        serviceRunning =false;
+        Log.d("service", "stop service called");
+        //Log.i(TAG, "Stopped service");
+        super.onDestroy();
+    }*/
+
+    public void updateGUI(Intent intent) {
+        if (intent.getExtras() != null) {
+            long millis = intent.getLongExtra("countdown", 0);
+            int done = intent.getIntExtra("done", 0);
+            int progress = (int) (millis/1000);
             progressBar.setProgress(progressBar.getMax()-progress);
 
             if (dobaPeceni == 0){//tzn byl zobrazen timePicker, takze neni vytvoren TextView
@@ -177,19 +270,21 @@ public class CountdownTimer extends Activity {
             Log.d("progress", String.valueOf(progress));
             if (progress <= 1){
                 //ring !! vibrate !! :)
-                manager.notify(1, builder.build());
-                Toast.makeText(getApplicationContext(), R.string.done, Toast.LENGTH_LONG).show();
+                //manager.notify(1, builder.build());
+
                 stopButton.setVisibility(View.INVISIBLE);
                 startButton.setVisibility(View.VISIBLE);
                 stopButton.setClickable(false);
                 startButton.setClickable(true);
                 dobaPeceniTV.setText("00:00");
+//                stopService(new Intent(this, MyService.class));
             }
-        }
-        //TODO: funkcni i po zavreni aplikace nebo switchnuti na jinou aktivitu !
-        @Override
-        public void onFinish() {
-            //finish();
+
+            if (done != 0) {
+                dobaPeceniTV.setText("00:00");
+            }
+            //Log.i(TAG, "Countdown seconds remaining: " +  millisUntilFinished / 1000);
         }
     }
+
 }
---
+++ app/src/main/java/com/example/erika/cookbook/MyService.java
@@ -0,0 +1,146 @@
+package com.example.erika.cookbook;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Timer;
+
+/**
+ * Created by Erika on 7. 1. 2017.
+ */
+
+public class MyService extends Service {
+    Bundle extras;
+    CountDownTimer timer = null;
+    int dobaPeceni;
+    public static String str_receiver = "com.example.erika.cookbook.receiver";
+    //Intent mIntent = new Intent(this, MyService.class);
+    Intent receiver;
+    public NotificationManager manager;
+    public NotificationCompat.Builder builder;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //extras = mIntent.getExtras();
+        receiver = new Intent(str_receiver);
+    }
+
+    @Override
+    public void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        extras = intent.getExtras();
+        if (extras != null){
+            Log.d("Extras delivered", String.valueOf(extras.getInt("doba_peceni")));
+            dobaPeceni = extras.getInt("doba_peceni");
+
+            Intent notificationIntent = new Intent(this, CountdownTimer.class);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                    notificationIntent, 0);
+
+            final Notification notification = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("Kuchařka")
+                    .setContentText("Běží odpočet času...")
+                    .setContentIntent(pendingIntent).build();
+
+            timer = new CountDownTimer(dobaPeceni, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    receiver.putExtra("countdown", millisUntilFinished);
+                    sendBroadcast(receiver);
+                }
+
+                @Override
+                public void onFinish() {
+                    Log.d("countdowntimer finished", "ding dong");
+                    builder = new NotificationCompat.Builder(MyService.this);
+                    long[] pattern = {500,500,500,500,500,500,500,500,500};
+                    builder.setVibrate(pattern);
+                    builder.setStyle(new NotificationCompat.InboxStyle());
+                    Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                    builder.setSound(alarmSound);
+                    manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    manager.notify(1, builder.build());
+                    Toast.makeText(getApplicationContext(), R.string.done, Toast.LENGTH_LONG).show();
+                    //updateNotification();
+                    Thread closeActivity = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Thread.sleep(5000);
+                                stopSelf();
+                            } catch (Exception e) {
+                                e.getLocalizedMessage();
+                            }
+                        }
+                    });
+                    closeActivity.start();
+                }
+            };
+            timer.start();
+
+
+
+            startForeground(1337, notification);
+        }
+
+        return START_STICKY;
+    }
+
+    private Notification getMyActivityNotification(String text){
+        // The PendingIntent to launch our activity if the user selects
+        // this notification
+        CharSequence title = "Kuchařka";
+        Intent notificationIntent = new Intent(this, CountdownTimer.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+
+        return new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setContentIntent(pendingIntent).getNotification();
+    }
+
+    /**
+     * This is the method that can be called to update the Notification
+     */
+    private void updateNotification() {
+        String text = "Odpočet hotov!";
+
+        Notification notification = getMyActivityNotification(text);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1337, notification);
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
